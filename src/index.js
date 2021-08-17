4ecc12f39f78bb849c2a5a6a287fbf87e5088459
Daria Malina <gluto68@gmail.com>
2021-08-17 20:06:23 +0300

    feat: implementation of specified functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length===0||arguments[0].length===0){
+        return 0;
+    } else {
+        return array.sort((a,b)=>a-b)[0]
+    }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length===0||arguments[0].length===0){
+        return 0;
+    } else {
+        return array.sort((a,b)=>a-b)[array.length-1]
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length===0||arguments[0].length===0){
+        return 0;
+    } else {
+        let sum= array.reduce(function(previousValue, currentValue) {
+            return previousValue + currentValue;
+        })
+        return sum/array.length
+    }
+
+
 }
